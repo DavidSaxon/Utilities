6307c345b9a5106b64d00ad0d6868acbfbc1b715
David Saxon <davidesaxon@gmail.com>
2013-05-01 04:17:45 +1200

    added radiansToDegrees and vice versa

--- ValuesUtil.hpp
+++ ValuesUtil.hpp
@@ -10,6 +10,8 @@ namespace util { namespace val {
 
 const unsigned BITS_IN_BYTE = 8;
 const unsigned BITS_IN_INT = 32;
+const float degreesToRadians = 0.0174532925;
+const float radiansToDegrees = 57.2957795;
 
 }} //util //val
 
