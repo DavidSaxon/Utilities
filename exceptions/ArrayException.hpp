6a6c7092e46f4569cc632d1b82080bb60f060183
David Saxon <davidesaxon@gmail.com>
2013-09-30 17:33:47 +1300

    index out of bounds exception

---
+++ exceptions/ArrayException.hpp
@@ -0,0 +1,53 @@
+/****************************************************\
+| Exceptions related to array (and list operations). |
+|                                                    |
+| @author David Saxon                                |
+\****************************************************/
+
+#ifndef UTILITIES_EXCEPTIONS_ARRAYEXCEPTION_H_
+#   define UTILITIES_EXCEPTIONS_ARRAYEXCEPTION_H_
+
+#include <iostream>
+#include <sstream>
+
+#include "Exception.hpp"
+
+namespace util { namespace ex {
+
+/***********************************************\
+| Abstract base class for all array exceptions. |
+|                                               |
+| @author David Saxon                           |
+\***********************************************/
+class ArrayException : public Exception {
+};
+
+/**********************************************\
+| Warns that the given index is out of bounds. |
+|                                              |
+| @author David Saxon                          |
+\**********************************************/
+class IndexOutOfBoundsException : public ArrayException {
+public:
+
+    //CONSTRUCTOR
+    /*!Creates a new index out of bounds exception
+    @_message the error message*/
+    IndexOutOfBoundsException(const std::string& _message) {
+
+        errMsg = _message;
+    }
+
+private:
+
+    //PRIVATE MEMBER FUNCTIONS
+    /*!@return the name of the exception*/
+    std::string name() const {
+
+        return "INDEX OUT OF BOUNDS EXCEPTION";
+    }
+};
+
+} } //util //ex
+
+#endif;
